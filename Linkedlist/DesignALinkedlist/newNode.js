99e0af47aac9569a7cee2d21e7d304761d8a3719
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-02-20 05:44:49 +0530

    Design linkedlist : creating new Node

---
+++ Linkedlist/DesignALinkedlist/newNode.js
@@ -0,0 +1,11 @@
+function Node(val) {
+    this.val = val;
+    this.next = null;
+    console.log("Created node with value: " + this.val);
+
+}
+Node(5);
+
+const newNode = new Node(10);
+console.log("The value of newNode is: " + newNode.val);
+console.log("The next property of newNode is: " + newNode.next);
