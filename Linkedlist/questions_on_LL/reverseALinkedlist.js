1ca298c7b2ec68e083e08b4cef0c71626bf81684
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-02-23 07:02:47 +0530

    reversing the linklist

---
+++ Linkedlist/questions_on_LL/reverseALinkedlist.js
@@ -0,0 +1,14 @@
+const reverseALinkedlist = (head) => {
+    let prev = null;
+    let curr = head;
+    while( curr ){
+        let temp = curr.next;
+
+        curr.next = prev;
+        prev = curr;
+        curr= temp;
+    }
+    return prev;
+};
+
+reverseALinkedlist(head);
