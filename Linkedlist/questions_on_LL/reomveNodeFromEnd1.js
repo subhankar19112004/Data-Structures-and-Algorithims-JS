13bf32c48df6d1c6d60db6b1478774ee462fb6ea
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-03-03 05:05:58 +0530

    Removing a node from a a given singly linked list from the end of the list.( this solution is two pass solution)

---
+++ Linkedlist/questions_on_LL/reomveNodeFromEnd1.js
@@ -0,0 +1,26 @@
+
+//Removing a node from a a given singly linked list from the end of the list.( this solution is one pass solution)
+const reomveNodeFromEnd1 = (head, n) => {
+
+    //creating a sentinel node to handle edge cases like removing the head node
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let prev = sentinel;
+
+    //first we will calculate the length of the linked list
+    let length = 0;
+    while (head) {
+        head = head.next;
+        length++;
+    }
+
+    //then we will calculate the position of the node to be removed from the start of the list
+    let prevPos = length - n;
+    for (let i = 0; i < prevPos; i++) {
+        prev = prev.next;
+    }
+    prev.next = prev.next.next;
+
+    //finally we will return the head of the modified linked list
+    return sentinel.next;
+}
