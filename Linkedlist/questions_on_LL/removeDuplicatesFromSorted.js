b3cbe55a55e177b9b486f0d69ea8dd578c54d515
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-03-04 04:24:58 +0530

    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.

---
+++ Linkedlist/questions_on_LL/removeDuplicatesFromSorted.js
@@ -0,0 +1,12 @@
+const removeDuplicatesFromSorted = (head) => {
+    let curr = head;
+    
+    while( curr && curr.next ){
+        if( curr.val == curr.next.val){
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};
