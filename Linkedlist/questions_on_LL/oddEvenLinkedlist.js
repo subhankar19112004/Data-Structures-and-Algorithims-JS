dfee9ec1cce71cdbb8b45c71afa442099a430e05
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-03-05 19:02:07 +0530

    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
    The first node is considered odd (1st position), the second is even (2nd position), and so on.

---
+++ Linkedlist/questions_on_LL/oddEvenLinkedlist.js
@@ -0,0 +1,16 @@
+const oddEvenLinkedlist = (head) => {
+    if( !head || !head.next ) return head;
+
+    let odd = head;
+    let even = head.next;
+    let evenStart = even;
+
+    while( odd.next && even.next ){
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+    odd.next = evenStart;
+    return head;
+}
