52843f132d10d4e667d81bc30aa244343ecb61ae
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-03-19 04:28:03 +0530

    Approach-3: We can also use one while loop to iterate through the string from the end and count the characters until we encounter a space. We can ignore any trailing spaces at the end of the string. Approach-2: We can also split the string into an array of words and return the length of the last word in the array.

--- Strings/lengthOfLastWord.js
+++ Strings/lengthOfLastWord.js
@@ -4,16 +4,16 @@
 const lengthOfLastWord = (s) => {
 
     let n = s.length - 1;
-    while(n >= 0 ){
-        if(s[n] !== " "){
+    while (n >= 0) {
+        if (s[n] !== " ") {
             break;
         }
         --n;
     }
 
     let count = 0;
-    while( n>=0){
-        if(s[n] === " "){
+    while (n >= 0) {
+        if (s[n] === " ") {
             break;
         }
         ++count;
@@ -23,5 +23,26 @@ const lengthOfLastWord = (s) => {
 
     return count;
 }
+
+//Approach-2: We can also split the string into an array of words and return the length of the last word in the array.
+const lengthOfLastWord2 = (s) => {
+    let words = s.trim().split(" ");
+    return words[words.length - 1].length;
+}
+
+//Approach-3: We can also use one while loop to iterate through the string from the end and count the characters until we encounter a space. We can ignore any trailing spaces at the end of the string.
+var lengthOfLastWord3 = function (s) {
+    let n = s.length - 1;
+    let count = 0;
+    while (n >= 0) {
+        if (s[n] != " ") {
+            ++count;
+        } else if (count > 0) {
+            break;
+        }
+        --n;
+    }
+    return count;
+};
 let s = "Hello World";
 console.log(lengthOfLastWord(s));
