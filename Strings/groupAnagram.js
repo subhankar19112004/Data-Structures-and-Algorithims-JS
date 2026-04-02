c30980bb1b037f655d99852edca5242d36c9b90d
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-04-03 05:19:44 +0530

    Group Anagrams

---
+++ Strings/groupAnagram.js
@@ -0,0 +1,24 @@
+//49. Group Anagrams
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// Approach: We can use a hash map to group the anagrams together. 
+// We iterate through each string in the input array, sort the characters of the string, and use the sorted string as a key in the hash map. 
+// If the key does not exist, we create a new entry with the original string as the first element of an array. 
+// If the key already exists, we push the original string into the existing array. 
+// Finally, we return the values of the hash map as an array of arrays.
+const groupAnagram = (strs) => {
+    let map = {};
+    for(let i = 0; i < strs.length; i++){
+        let sortedStr = strs[i].split("").sort().join("");
+        if(!map[sortedStr]){
+            map[sortedStr] = [strs[i]];
+        } else {
+            map[sortedStr].push(strs[i]);
+        }
+    }
+    return [...Object.values(map)];
+}
+
+let strs = ["eat","tea","tan","ate","nat","bat"];
+console.log(groupAnagram(strs));
+
