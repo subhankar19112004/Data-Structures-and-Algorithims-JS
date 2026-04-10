9748582566dc63e330dfc2a4d509ea29ba8563b9
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-04-11 04:58:26 +0530

    Implement stack using one queue

--- Stacks_and_Queues/stackUsingQueue.js
+++ Stacks_and_Queues/stackUsingQueue.js
@@ -1,4 +1,5 @@
-
+// Implement stack using two queues. The operations of stack should be supported by using only the standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+// Applying the above operations, implement a last-in-first-out (LIFO) stack using queues. You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
 var MyStack = function () {
     this.q1 = [];
     this.q2 = [];
@@ -62,3 +63,52 @@ MyStack.prototype.empty = function () {
  * var param_3 = obj.top()
  * var param_4 = obj.empty()
  */
+
+
+// Implement stack using one queue. The operations of stack should be supported by using only the standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+
+
+var MyStack = function() {
+    this.q1 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.q1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    let n = this.q1.length;
+    for(let i = 0; i < n - 1; i++){
+        this.q1.push(this.q1.shift());
+    }
+    let ans = this.q1.shift();
+    return ans;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    let n = this.q1.length;
+    for(let i = 0; i < n - 1; i++){
+        this.q1.push(this.q1.shift());
+    }
+    let ans = this.q1.shift();
+    this.q1.push(ans);
+    return ans;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q1.length === 0;
+};
+
