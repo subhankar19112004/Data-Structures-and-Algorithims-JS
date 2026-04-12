14b763b15819dfe052b9f74c921aa02b524d7c2a
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-04-12 06:53:21 +0530

    Corrected Empty fuction

--- Stacks_and_Queues/queueUsingStack.js
+++ Stacks_and_Queues/queueUsingStack.js
@@ -42,7 +42,7 @@ MyQueue.prototype.peek = function () {
  * @return {boolean}
  */
 MyQueue.prototype.empty = function () {
-  return this.s1.length && this.s2.length === 0;
+  return this.s1.length === 0 && this.s2.length === 0;
 };
 
 /**
