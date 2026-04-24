450d0f5779c1b8e25321ceccd634553bb9275674
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-04-24 06:09:36 +0530

    leetcode 496. Next Greater Element I

---
+++ Stacks_and_Queues/nextGreaterEl.js
@@ -0,0 +1,48 @@
+// leetcode 496. Next Greater Element I
+// The next greater element of some element x in an array is the first greater element that is to
+//  the right of x in the same array. You are given two distinct 0-indexed integer arrays nums1 and
+//  nums2, where nums1 is a subset of nums2.
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine
+//  the next greater element of nums2[j] in nums2. If there is no next greater element, then the
+//  answer for this query is -1. Return an array ans of length nums1.length such that ans[i] is
+//  the next greater element as described above.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, arr) {
+    let ngeMap = {};
+    let stack = [];
+    let n = arr.length;
+    stack.push(arr[n - 1]);
+    ngeMap[arr[n - 1]] = -1;
+
+    for (let i = n - 2; i >= 0; i--) {
+        let top = stack[stack.length - 1];
+        if (arr[i] < top) {
+            ngeMap[arr[i]] = top;
+        } else {
+            while (stack.length) {
+                if (arr[i] > stack[stack.length - 1]) {
+                    stack.pop();
+                } else {
+                    ngeMap[arr[i]] = stack[stack.length - 1];
+                    break;
+                }
+            }
+            if(!stack.length){
+                ngeMap[arr[i]] = -1;
+            }
+        }
+        stack.push(arr[i]);
+    }
+
+    let ans = [];
+    for(let i = 0; i < nums1.length; i++){
+        ans.push(ngeMap[nums1[i]]);
+    }
+
+    return ans;
+};
