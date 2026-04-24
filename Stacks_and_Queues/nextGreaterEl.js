9796d29ad461d432bffb06f4248388b64d6da0ee
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-04-24 06:10:18 +0530

    leetcode 496. Next Greater Element I

--- Stacks_and_Queues/nextGreaterEl.js
+++ Stacks_and_Queues/nextGreaterEl.js
@@ -46,3 +46,7 @@ var nextGreaterElement = function (nums1, arr) {
 
     return ans;
 };
+
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4]));
+console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]));
