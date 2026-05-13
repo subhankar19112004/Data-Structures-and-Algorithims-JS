b2c799679901195d95260d838c4475f4011653aa
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-05-13 19:10:15 +0530

    Leetcode: 34. Find First and Last Position of Element in Sorted Array

---
+++ Seraching/BinarySearch/searchRange.js
@@ -0,0 +1,47 @@
+// Leetcode: 34. Find First and Last Position of Element in Sorted Array
+// Given an array of integers nums sorted in non-decreasing order, 
+// find the starting and ending position of a given target value.
+// If target is not found in the array, return [-1, -1].
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+const searchRange = (arr, target) => {
+    if (arr.length == 1 && target == arr[0]) return [0, 0];
+    let left = 0;
+    let right = arr.length - 1;
+
+    let ans = [-1, -1];
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    arr[left] === target ? ans[0] = left : -1;
+
+    left = 0;
+    right = arr.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.ceil((right - left) / 2);
+
+        if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid;
+        }
+    }
+    arr[left] === target ? ans[1] = left : -1;
+
+    return ans;
+}
+
+// Test cases
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6));
+console.log(searchRange([], 0));
+console.log(searchRange([1], 1));
