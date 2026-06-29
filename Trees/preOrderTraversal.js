e16106f57dde2df86059a6b219ec7d97e752e736
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-06-29 09:19:51 +0530

    LeetCode : 144. Binary Tree Preorder Traversal

---
+++ Trees/preOrderTraversal.js
@@ -0,0 +1,42 @@
+// LeetCode : 144. Binary Tree Preorder Traversal
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+// Tree structure:
+//     1
+//   /   \
+// null   2
+//      /
+//     3
+
+
+//Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  let ans = [];
+
+  function traversal(curr) {
+    if (!curr) return;
+    ans.push(curr.val);
+
+    traversal(curr.left);
+    traversal(curr.right);
+  }
+
+  traversal(root);
+
+  return ans;
+};
+
+// Test case
+const root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+console.log(preorderTraversal(root)); // Output: [1, 2, 3]
