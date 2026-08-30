6fa6f5a6ce654e847400f79b76aac9545e24fb37
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-08-30 08:35:38 +0530

    Merge sort Algorithm

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -1,4 +1,10 @@
-function merge (nums1, nums2){
+// Merge sort Algorithm implementation in JavaScript
+// It is a Divide and Conquer algorithm that divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves.
+// Time Complexity: O(n log n) in all cases (worst, average, best)
+
+
+// Helper function to merge two sorted arrays into one sorted array using two pointers
+function merge(nums1, nums2) {
     let m = nums1.length;
     let n = nums2.length;
     let p1 = 0;
@@ -30,9 +36,33 @@ function mergeSort(arr){
     let right = mergeSort(arr.slice(mid));
 
     // Merge the sorted halves and return the result
-    return merge( left, right); //merge -> Helper function to merge two sorted arrays
+    return mergesort(left, right); //merge -> Helper function to merge two sorted arrays
 }
 
 let arr = [5, 2, 9, 1, 5, 6];
+let arr2 = [7, 5, 3, 4, 2, 1];
 let result = mergeSort(arr);
+let result2 = mergeSort(arr2);
 console.log(result);// Output: [1, 2, 5, 5, 6, 9]
+console.log(result2);// Output: [1, 2, 3, 4, 5, 7]
+
+
+// Merge sort helper function using while loop
+function mergesort(nums1, nums2) {
+    let m = nums1.length;
+    let n = nums2.length;
+    let i = 0;
+    let j = 0;
+    let res = [];
+
+    while (i < m && j < n) {
+        if( nums1[i] < nums2[j]) {
+            res.push(nums1[i]);
+            i++;
+        } else {
+            res.push(nums2[j]);
+            j++;
+        }
+    }
+    return [...res, ...nums1.slice(i), ...nums2.slice(j)];
+}
